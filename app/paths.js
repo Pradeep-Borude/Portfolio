b7e6e22e8db580a3ecb6f08df41d4f059fc7f3b7
procoder406 <pradeepborude406@gmail.com>
2025-07-27 18:45:27 +0530

    completed

--- app/paths.js
+++ app/paths.js
@@ -1,24 +1,24 @@
 export const Projectvideos = [
   {
-    src: "public/videos/spotify.mp4",
+    src: "/videos/spotify.mp4",
     link: "https://github.com/Pradeep-Borude/spotify-clone",
     title: "Spotify Clone",
     // desc: "Sleek music UI & playback",
     desc: "I built a responsive music streaming UI inspired by Spotify using HTML,  CSS, and JavaScript . It features interactive controls, and a clean layout designed to mimic the Spotify experience.",
-    bgsrc: "public/images/music.jpg"
+    bgsrc: "/images/music.jpg"
   },
   {
-    src: "public/videos/todoList.mp4",
+    src: "/videos/todoList.mp4",
     link: "https://github.com/Pradeep-Borude/Todo_List",
     title: "To-Do List",
     desc:"I developed a clean and minimal todo list application using React, Tailwind CSS. It allows users to add, edit, and delete tasks, ensuring task persistence even after refresh.",
-    bgsrc: "public/images/todo.jpg"
+    bgsrc: "/images/todo.jpg"
   },
   {
-    src: "public/videos/passop.mp4",
+    src: "/videos/passop.mp4",
     link: "https://github.com/Pradeep-Borude/PassOP--your-own-password-manager",
     title: "Password Manager",
     desc:"PassOP is a  user-friendly password manager with clean layout. It allows users to add, reveal, and delete password , ensuring task persistence even after refresh.",
-    bgsrc: "public/images/security.jpg"
+    bgsrc: "/images/security.jpg"
   },
 ];
